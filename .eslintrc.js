3c5e96501ae4b3accf9de559bc3bae3f564221e4
Erin Joost <ecjoost@pbs.org>
2021-05-04 10:57:42 -0400

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'extends': '@pbs/eslint-config-pbs-kids',
+    'env': {
+        'node': true,
+        'es6': true,
+        'es2020': true 
+    }
+};
---
+++ scripts/zip_lambda.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+npm ci --only=prod 
+rm ./lambda.zip
+
+echo "making lambda zip....."
+mkdir temp
+cp -r lambda/. temp
+cp -r node_modules/ temp/node_modules/
+cp -r package.json temp/package.json
+
+time='date +%s'
+cd temp
+zip -rq ../lambda.zip .
+cd ..
+
+rm -rf temp/
+
+npm ci
